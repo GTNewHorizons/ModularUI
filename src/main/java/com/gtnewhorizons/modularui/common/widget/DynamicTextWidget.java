dbea89c5e7361584938a9ae0c2e85ddf7f41f99e
miozune <miozune@gmail.com>
2022-09-20 13:55:24 +0900

    Allow setting default color for DynamicTextWidget

--- src/main/java/com/gtnewhorizons/modularui/common/widget/DynamicTextWidget.java
+++ src/main/java/com/gtnewhorizons/modularui/common/widget/DynamicTextWidget.java
@@ -2,10 +2,14 @@ package com.gtnewhorizons.modularui.common.widget;
 
 import com.gtnewhorizons.modularui.api.drawable.Text;
 import java.util.function.Supplier;
+import net.minecraft.util.EnumChatFormatting;
 
 public class DynamicTextWidget extends TextWidget {
 
     private final Supplier<Text> textSupplier;
+
+    private Integer defaultColor;
+    private EnumChatFormatting defaultFormat;
 
     public DynamicTextWidget(Supplier<Text> text) {
         this.textSupplier = text;
@@ -22,6 +26,25 @@ public class DynamicTextWidget extends TextWidget {
 
     @Override
     public Text getText() {
-        return textSupplier.get();
+        Text ret = textSupplier.get();
+        if (defaultColor != null) {
+            ret.color(defaultColor);
+        }
+        if (defaultFormat != null) {
+            ret.format(defaultFormat);
+        }
+        return ret;
+    }
+
+    @Override
+    public TextWidget setDefaultColor(int color) {
+        this.defaultColor = color;
+        return this;
+    }
+
+    @Override
+    public TextWidget setDefaultColor(EnumChatFormatting color) {
+        this.defaultFormat = color;
+        return this;
     }
 }
