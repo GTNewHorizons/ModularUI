3fd3798e033e3bf67a78109ac60c9dc3df49c6e9
miozune <miozune@gmail.com>
2022-11-11 21:30:26 +0900

    Allow dynamic Drawable for DrawableWidget

--- src/main/java/com/gtnewhorizons/modularui/common/widget/DrawableWidget.java
+++ src/main/java/com/gtnewhorizons/modularui/common/widget/DrawableWidget.java
@@ -8,6 +8,8 @@ import com.gtnewhorizons.modularui.api.drawable.Text;
 import com.gtnewhorizons.modularui.api.drawable.UITexture;
 import com.gtnewhorizons.modularui.api.math.Pos2d;
 import com.gtnewhorizons.modularui.api.widget.Widget;
+import java.util.function.Supplier;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -15,8 +17,8 @@ import org.jetbrains.annotations.Nullable;
  */
 public class DrawableWidget extends Widget {
 
-    @Nullable
-    private IDrawable drawable = IDrawable.EMPTY;
+    @NotNull
+    private Supplier<IDrawable> drawable = () -> IDrawable.EMPTY;
 
     @Override
     public void readJson(JsonObject json, String type) {
@@ -28,32 +30,37 @@ public class DrawableWidget extends Widget {
 
     @Override
     public void onScreenUpdate() {
-        if (drawable != null) {
-            drawable.tick();
+        if (drawable.get() != null) {
+            drawable.get().tick();
         }
     }
 
     @Override
     public void draw(float partialTicks) {
-        if (drawable != null) {
+        if (drawable.get() != null) {
             GlStateManager.pushMatrix();
             // so that item z levels are properly ordered
             // todo: probably need better solution so that more windows can work
             GlStateManager.translate(0, 0, 150 * getWindowLayer());
-            drawable.draw(Pos2d.ZERO, getSize(), partialTicks);
+            drawable.get().draw(Pos2d.ZERO, getSize(), partialTicks);
             GlStateManager.popMatrix();
         }
     }
 
     @Nullable
     public IDrawable getDrawable() {
-        return drawable;
+        return drawable.get();
     }
 
-    public DrawableWidget setDrawable(IDrawable drawable) {
+    public DrawableWidget setDrawable(@Nullable IDrawable drawable) {
         if (drawable instanceof Text) {
             ModularUI.logger.warn("Please use TextWidget for Text");
         }
+        this.drawable = () -> drawable;
+        return this;
+    }
+
+    public DrawableWidget setDrawable(@NotNull Supplier<IDrawable> drawable) {
         this.drawable = drawable;
         return this;
     }
