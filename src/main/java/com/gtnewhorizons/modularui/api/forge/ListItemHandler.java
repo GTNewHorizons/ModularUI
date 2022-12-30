de3bd7c10bea54ac72cdb1b4da4cf93c8657c421
miozune <miozune@gmail.com>
2022-12-31 01:31:11 +0900

    Make ListItemHandler accept only IItemHandlerModifiable

--- src/main/java/com/gtnewhorizons/modularui/api/forge/ListItemHandler.java
+++ src/main/java/com/gtnewhorizons/modularui/api/forge/ListItemHandler.java
@@ -6,21 +6,21 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
 /**
- * Wraps list of {@link IItemHandler} into one handler.
+ * Wraps list of {@link IItemHandlerModifiable} into one handler.
  */
 @SuppressWarnings("unused")
 public class ListItemHandler implements IItemHandlerModifiable {
 
-    protected final Iterable<? extends IItemHandler> listItemHandler;
+    protected final Iterable<? extends IItemHandlerModifiable> listItemHandler;
 
-    public ListItemHandler(Iterable<? extends IItemHandler> listItemHandler) {
+    public ListItemHandler(Iterable<? extends IItemHandlerModifiable> listItemHandler) {
         this.listItemHandler = listItemHandler;
     }
 
     @Override
     public int getSlots() {
         int ret = 0;
-        for (IItemHandler itemHandler : listItemHandler) {
+        for (IItemHandlerModifiable itemHandler : listItemHandler) {
             ret += itemHandler.getSlots();
         }
         return ret;
@@ -28,41 +28,39 @@ public class ListItemHandler implements IItemHandlerModifiable {
 
     @Override
     public ItemStack getStackInSlot(int slot) {
-        Pair<? extends IItemHandler, Integer> result = findItemHandler(slot);
+        Pair<? extends IItemHandlerModifiable, Integer> result = findItemHandler(slot);
         return result.getLeft().getStackInSlot(result.getRight());
     }
 
     @Nullable
     @Override
     public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
-        Pair<? extends IItemHandler, Integer> result = findItemHandler(slot);
+        Pair<? extends IItemHandlerModifiable, Integer> result = findItemHandler(slot);
         return result.getLeft().insertItem(result.getRight(), stack, simulate);
     }
 
     @Nullable
     @Override
     public ItemStack extractItem(int slot, int amount, boolean simulate) {
-        Pair<? extends IItemHandler, Integer> result = findItemHandler(slot);
+        Pair<? extends IItemHandlerModifiable, Integer> result = findItemHandler(slot);
         return result.getLeft().extractItem(result.getRight(), amount, simulate);
     }
 
     @Override
     public int getSlotLimit(int slot) {
-        Pair<? extends IItemHandler, Integer> result = findItemHandler(slot);
+        Pair<? extends IItemHandlerModifiable, Integer> result = findItemHandler(slot);
         return result.getLeft().getSlotLimit(result.getRight());
     }
 
     @Override
     public void setStackInSlot(int slot, ItemStack stack) {
-        Pair<? extends IItemHandler, Integer> result = findItemHandler(slot);
-        if (result.getLeft() instanceof IItemHandlerModifiable) {
-            ((IItemHandlerModifiable) result.getLeft()).setStackInSlot(result.getRight(), stack);
-        }
+        Pair<? extends IItemHandlerModifiable, Integer> result = findItemHandler(slot);
+        result.getLeft().setStackInSlot(result.getRight(), stack);
     }
 
     @Override
     public boolean isItemValid(int slot, ItemStack stack) {
-        Pair<? extends IItemHandler, Integer> result = findItemHandler(slot);
+        Pair<? extends IItemHandlerModifiable, Integer> result = findItemHandler(slot);
         return result.getLeft().isItemValid(result.getRight(), stack);
     }
 
@@ -71,9 +69,9 @@ public class ListItemHandler implements IItemHandlerModifiable {
      * @param slot Index to search
      * @return Pair of item handler and actual index for it
      */
-    protected Pair<? extends IItemHandler, Integer> findItemHandler(int slot) {
+    protected Pair<? extends IItemHandlerModifiable, Integer> findItemHandler(int slot) {
         int searching = 0;
-        for (IItemHandler itemHandler : listItemHandler) {
+        for (IItemHandlerModifiable itemHandler : listItemHandler) {
             int numSlots = itemHandler.getSlots();
             if (slot >= searching && slot < searching + numSlots) {
                 return new ImmutablePair<>(itemHandler, slot - searching);
