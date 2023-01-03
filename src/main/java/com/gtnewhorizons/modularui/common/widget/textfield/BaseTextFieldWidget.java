d6fd419d3f656a20977a69683c03e1ec6fa30862
miozune <miozune@gmail.com>
2023-01-03 20:38:17 +0900

    Fix arbitrary string being accepted for DECIMALS

--- src/main/java/com/gtnewhorizons/modularui/common/widget/textfield/BaseTextFieldWidget.java
+++ src/main/java/com/gtnewhorizons/modularui/common/widget/textfield/BaseTextFieldWidget.java
@@ -49,8 +49,7 @@ public class BaseTextFieldWidget extends Widget implements IWidgetParent, Intera
      */
     public static final Pattern WHOLE_NUMS = Pattern.compile("-?[0-9]*([+\\-*/%^][0-9]*)*");
 
-    public static final Pattern DECIMALS = Pattern.compile(
-            "[0-9]*(" + getDecimalSeparator() + "[0-9]*)?([+\\-*/%^][0-9]*(" + getDecimalSeparator() + "[0-9]*)?)*");
+    public static final Pattern DECIMALS = Pattern.compile("[0-9]*(\\.[0-9]*)?([+\\-*/%^][0-9]*(\\.[0-9]*)?)*");
     /**
      * alphabets
      */
