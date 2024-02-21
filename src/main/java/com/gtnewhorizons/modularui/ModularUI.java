391a090c46b2b8e1410bdc2320cc20cc5b837c22
Abdiel Kavash <19243993+AbdielKavash@users.noreply.github.com>
2024-02-21 00:15:02 -0600

    Added a numeric widget. (#62)
    * Added NumericWidget.
    
    * Added localization support for displaying numbers.
    
    * Fixes + deps.
    
    * Fix frenchn't
    
    * Made field final.

--- src/main/java/com/gtnewhorizons/modularui/ModularUI.java
+++ src/main/java/com/gtnewhorizons/modularui/ModularUI.java
@@ -41,6 +41,7 @@ public class ModularUI {
     public static final String DEPENDENCIES = "required-after:gtnhmixins@[2.0.1,); "
             + "required-after:NotEnoughItems@[2.3.50-GTNH,);"
             + "after:hodgepodge@[2.0.0,);"
+            + "after:gtnhlib@[0.2.7,);"
             + "before:gregtech";
     public static final String GUI_FACTORY = "com.gtnewhorizons.modularui.config.GuiFactory";
 
@@ -51,6 +52,7 @@ public class ModularUI {
     public static final boolean isGT5ULoaded = Loader.isModLoaded(MODID_GT5U) && !Loader.isModLoaded(MODID_GT6);
     public static final boolean isHodgepodgeLoaded = Loader.isModLoaded("hodgepodge");
     public static final boolean isAE2Loaded = Loader.isModLoaded("appliedenergistics2");
+    public static final boolean isGTNHLibLoaded = Loader.isModLoaded("gtnhlib");
 
     public static final boolean isDevEnv = (boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment");
 
